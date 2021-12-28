8ec76bd4b1f0041a80026cd321845ece41ec891b
Jack Slinger <jackslinger10@gmail.com>
2021-12-28 13:53:33 +0000

    Day 5 part 2

--- day5/day5.rb
+++ day5/day5.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 
+require "pry"
+
 # file = File.open("day5/example.txt").read.split("\n")
 file = File.open("day5/input.txt").read.split("\n")
+
+Point = Struct.new(:x, :y)
 
 class Line
   attr_reader :x1, :y1, :x2, :y2
@@ -10,6 +14,33 @@ class Line
     one, two = input_string.split(" -> ")
     @x1, @y1 = one.split(",").map(&:to_i)
     @x2, @y2 = two.split(",").map(&:to_i)
+  end
+
+  def cells
+    if vertical?
+      (min_y...(max_y + 1)).map do |y|
+        Point.new(x1, y)
+      end
+    elsif horizontal?
+      (min_x...(max_x + 1)).map do |x|
+        Point.new(x, y1)
+      end
+    else
+      x_coords = if x1 < x2
+        (x1..x2).to_a
+      else
+        (x2..x1).to_a.reverse
+      end
+      y_coords = if y1 < y2
+        (y1..y2).to_a
+      else
+        (y2..y1).to_a.reverse
+      end
+
+      x_coords.zip(y_coords).map do |x, y|
+        Point.new(x, y)
+      end
+    end
   end
 
   def orthogonal?
@@ -48,10 +79,11 @@ end
 class Grid
   attr_reader :width, :height, :lines
 
-  def initialize(lines)
+  def initialize(lines, diagonals = false)
     @lines = lines
     @width = lines.map(&:max_x).max + 1
     @height = lines.map(&:max_y).max + 1
+    @diagonals = diagonals
   end
 
   def numbers
@@ -62,15 +94,10 @@ class Grid
     end
 
     lines.each do |line|
-      if line.vertical?
-        (line.min_y...(line.max_y + 1)).each do |y|
-          @numbers[y][line.x1] += 1
+      if line.orthogonal? || @diagonals == true
+        line.cells.each do |point|
+          @numbers[point.y][point.x] += 1
         end
-      elsif line.horizontal?
-        (line.min_x...(line.max_x + 1)).each do |x|
-          @numbers[line.y1][x] += 1
-        end
-      else
       end
     end
     @numbers
@@ -96,5 +123,9 @@ lines = file.map do |line|
 end
 grid = Grid.new(lines)
 
-# puts grid
+puts "Part 1"
+puts "Overlaps: #{grid.num_of_overlaps}"
+
+puts "\nPart 2"
+grid = Grid.new(lines, true)
 puts "Overlaps: #{grid.num_of_overlaps}"
